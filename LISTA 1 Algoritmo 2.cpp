1e7397d053176cfda19f463c3d38bfb51298742f
Ementonces <121621928+Ementonces@users.noreply.github.com>
2024-03-13 12:19:48 -0300

    Update LISTA 1 Algoritmo 2.cpp

--- LISTA 1 Algoritmo 2.cpp
+++ LISTA 1 Algoritmo 2.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
     int AnosFumando, CigarrosPorDia;
-    float GastoTotal, preço;
+    float GastoTotal, presso;
     cout << "\tA quantos anos você fuma?\n";
     cin >> AnosFumando;
     cout << "\tquantos cigarros você fuma por dia?\n";
     cin >> CigarrosPorDia;
     cout << "\tqual o preço da carteira que você compra?\n";
-    cin >> preço;
-    GastoTotal = AnosFumando * 365 * CigarrosPorDia * (preço / 20);
+    cin >> presso;
+    GastoTotal = AnosFumando * 365 * CigarrosPorDia * (presso / 20);
     cout << "\t" << GastoTotal << " reais seus viraram fumaça";
 }
