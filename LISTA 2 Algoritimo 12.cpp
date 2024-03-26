eb3467dca97f454de8714297fc1d15d7bcf86bca
Ementonces <121621928+Ementonces@users.noreply.github.com>
2024-03-26 12:59:56 -0300

    Add files via upload

---
+++ LISTA 2 Algoritimo 12.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int idade, anosT;
+  cin >> idade;
+  cin >> anosT;
+  if(idade >= 65){
+      
+  }
+  if(anosT >= 30){
+      
+  }
+  if(idade >= 60 && anosT >= 25){
+      
+    }
+}
