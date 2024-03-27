1f8b38792f531114bf35161932571026cffc12ef
Ementonces <121621928+Ementonces@users.noreply.github.com>
2024-03-27 12:20:07 -0300

    Update LISTA 2 Algoritimo 12.cpp

--- LISTA 2 Algoritimo 12.cpp
+++ LISTA 2 Algoritimo 12.cpp
@@ -3,15 +3,29 @@ using namespace std;
 
 int main(){
   int idade, anosT;
+  cout << "\tinsira a sua idade: ";
   cin >> idade;
+  cout << "\tinsira a quantos anos você trabalha: ";
   cin >> anosT;
-  if(idade >= 65){
-      
-  }
-  if(anosT >= 30){
-      
-  }
-  if(idade >= 60 && anosT >= 25){
-      
+  if(anosT > idade){
+      cout << "\tvire gente Eugênio";
+    }
+else{
+    if(idade >= 65){
+      cout << "\tvocê pode solicitar a aposentadoria";
+    }
+    else{
+        if(anosT >= 30){
+        cout << "\tvocê pode solicitar a aposentadoria";
+    }
+         else{
+                if(idade >= 60 && anosT >= 25){
+                cout << "\tvocê pode solicitar a aposentadoria";
+                }
+                else{
+                    cout << "\tvoc~e não pode solicitar a aposentadoria";
+                }
+            }
+        }
     }
 }
