1a9d4ac88d6b813a26ee64c4d05ec975312f462c
Ementonces <121621928+Ementonces@users.noreply.github.com>
2024-03-27 13:01:11 -0300

    Add files via upload

---
+++ LISTA 2 Algoritimo 14.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int num1, num2, num3;
+cin >> num1;
+cin >> num2;
+cin >> num3;
+if(num1 == num2 && num3 == num2){
+    
+}
+else{
+    if(num1 == num2 || num2 == num3 || num3 == num1){
+        
+    }
+    else{
+        
+    }
+}
+}
