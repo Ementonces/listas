71b234c889af652e949c2cc5c910d5ada0cfe7d2
Ementonces <121621928+Ementonces@users.noreply.github.com>
2024-03-27 21:05:55 -0300

    Update LISTA 2 Algoritimo 14.cpp

--- LISTA 2 Algoritimo 14.cpp
+++ LISTA 2 Algoritimo 14.cpp
@@ -3,18 +3,17 @@ using namespace std;
 
 int main(){
 int num1, num2, num3;
-cin >> num1;
-cin >> num2;
-cin >> num3;
+cout << "digite tres numeros inteiros: ";
+cin >> num1 >> num2 >> num3;
 if(num1 == num2 && num3 == num2){
-    
+    cout << "Todos os números são iguais";
 }
 else{
     if(num1 == num2 || num2 == num3 || num3 == num1){
-        
+        cout << "Apenas dois números são iguais";
     }
     else{
-        
+        cout << "Todos os números são diferentes";
     }
 }
 }
