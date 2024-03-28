6e55e4d708a4710cfdf91f3a9f2b3bf961543ad4
Ementonces <121621928+Ementonces@users.noreply.github.com>
2024-03-27 21:21:46 -0300

    Update LISTA 1 Algoritimo 10.cpp

--- LISTA 1 Algoritimo 10.cpp
+++ LISTA 1 Algoritimo 10.cpp
@@ -5,8 +5,15 @@ main ()
 {
 int a, b;
 bool c, d;
-cin >> a;
-cin >> b;
-cout << a + b << " " << a - b << " " << a * b << " " << a / b;
-
+cout << "insira dois valores inteiros: ";
+cin >> a >> b;
+cout << "insira dois valores booleanos (1 - verdadeiro / 0 - falso): ";
+cin >> c >> d;
+cout << "\n\tVALORES INTEIROS\n\tO valor da soma é: " << a + b ;
+cout << "\n\tO valor da subtração é: " << a - b ;
+cout << "\n\tO valor da multiplicação é: " << a * b ;
+cout << "\n\tO valor da divisão é: " << ((float)a / b);
+cout << "\n\tVALORES BOOLEANOS\n\tO valor da negação dos valores são: " << !c << " é " << !d;
+cout << "\n\tO valor da conjunção dos volares é: " << (c&&d);
+cout << "\n\tO valor da disjunção é: " << (c||d);
 }
