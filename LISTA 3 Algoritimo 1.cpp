0ed62240ba2849a613313a981f2eef3f38df2edd
Ementonces <121621928+Ementonces@users.noreply.github.com>
2024-04-10 13:10:50 -0300

    Add files via upload

---
+++ LISTA 3 Algoritimo 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int cod, codV, id, idV = 0, idN = 100, rep = 0;
+/*cod = codigo, codV codigo do mais velho,id = idVde, 
+idN = idVde do mais novo e rep = repetição.*/
+while(rep != 100){
+    cout << "\tinsira seu codigo: ";
+    cin >> cod;
+    cout << "\tinsira sua idade: ";
+    cin >> id;
+    if(id >= idV){
+        idV = id;
+        codV = cod;
+    }else{
+        if (id <= idN){
+            idN = id;
+        }
+    }
+    ++rep;
+    system("clear");
+}
+cout << "\tA idade da pessoa mais nova é " << idN << "\n\te o codigo da pessoa mais velha é " << codV;
+}
